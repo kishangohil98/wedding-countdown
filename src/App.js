35119d77e578a55beace2223ff2b398c0e7a0253
Kishan Gohil <gohilkishan159@gmail.com>
2025-02-04 19:35:09 +0530

    Wish added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
-import Birthday from './Birthday';
-import { Route, Switch } from 'react-router-dom';
-import RouterBirthday from './RouterBirthday';
-import Generate from './Generate';
+import React from "react";
+import "./App.css";
+import Main from "./Main";
+import { Route, Switch } from "react-router-dom";
+import RouterBirthday from "./RouterBirthday";
+import Generate from "./Generate";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Switch>
-        <Route exact path='/' component={Birthday} />
+        <Route exact path="/" component={Main} />
         <Route
           exact
-          path='/birthday/:name?/:day?/:month?'
+          path="/birthday/:name?/:day?/:month?"
           component={RouterBirthday}
         />
-        <Route exact path='/generate' component={Generate} />
+        <Route exact path="/generate" component={Generate} />
       </Switch>
     </div>
   );
